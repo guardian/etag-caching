guardian/etag-caching

commit fa16dd6c16faaab1165ab6914aafd1aa6155d699

Fix `InvocationTargetException` in ScalaDoc gen
This sbt command was failing with an `InvocationTargetException`:

```
+aws-s3-sdk-v2 / Compile / doc
```

...the 'doc' command is invoked during release, so we need it to work!

This is a known issue: https://github.com/lampepfl/dotty/issues/15288

See also https://github.com/scanamo/scanamo/pull/1643, which looks like
it fixed the issue by downgrading to Scala 3.2.0. Weirdly, this fix did
not seem to work for me when I tried it here.

build.sbt
@@ -26,7 +26,9 @@ def awsS3WithSdkVersion(version: Int)=
       libraryDependencies ++= Seq(
         awsSdkForVersion(version),
         "com.adobe.testing" % "s3mock-testcontainers" % "3.0.1" % Test
-      )
+      ),
+      Compile / doc / sources := // https://github.com/lampepfl/dotty/issues/15288, see also https://github.com/scanamo/scanamo/pull/1643
+        { if (scalaVersion.value.startsWith("3.")) Nil else (Compile / doc / sources).value }
     )
 
 val awsSdkForVersion = Map(

commit 30e47c22c6c4c4112983481f40d8f09573ccc8e1

Remove workaround for old bug in ScalaDoc gen
https://github.com/guardian/etag-caching/commit/fa16dd6c16faaab1165ab6914aafd1aa6155d699
introduced a workaround for a ScalaDoc bug - but that bug has been fixed
as of Scala 3.3.1:

https://github.com/lampepfl/dotty/pull/16882/commits/f77069a74c4df65c7f69a6476aeb6b52fa8acb48

We upgraded to Scala 3.3.1 with https://github.com/guardian/etag-caching/pull/12,
so we can remove the workaround.

build.sbt
@@ -32,9 +32,7 @@ def awsS3WithSdkVersion(version: Int)=
       libraryDependencies ++= Seq(
         awsSdkForVersion(version),
         "com.adobe.testing" % "s3mock-testcontainers" % "3.1.0" % Test
-      ),
-      Compile / doc / sources := // https://github.com/lampepfl/dotty/issues/15288, see also https://github.com/scanamo/scanamo/pull/1643
-        { if (scalaVersion.value.startsWith("3.")) Nil else (Compile / doc / sources).value }
+      )
     )
 
 val awsSdkForVersion = Map(