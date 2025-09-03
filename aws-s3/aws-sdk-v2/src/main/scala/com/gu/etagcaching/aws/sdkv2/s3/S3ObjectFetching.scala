e75cbd508b6deba6b03bf9372be5049b1c04bf50
Roberto Tyley <roberto.tyley@gmail.com>
2025-09-03 16:47:54 +0100

    Rearrange ExecutionContexts

--- aws-s3/aws-sdk-v2/src/main/scala/com/gu/etagcaching/aws/sdkv2/s3/S3ObjectFetching.scala
+++ aws-s3/aws-sdk-v2/src/main/scala/com/gu/etagcaching/aws/sdkv2/s3/S3ObjectFetching.scala
@@ -15,13 +15,22 @@ import scala.concurrent.{ExecutionContext, Future}
 
 case class S3ObjectFetching[Response](s3Client: S3AsyncClient, transformer: Transformer[Response])
   extends Fetching[ObjectId, Response] {
+  /**
+   * *All* Future-mapping occurring **in this particular class** performs very limited non-blocking computation.
+   * Hence, we can make creation & usage of this class easier by supplying a minimal 'parasitic' ExecutionContext,
+   * rather than asking the user of the class to provide an ExecutionContext.
+   *
+   * See also: https://tersesystems.com/blog/2024/06/20/executioncontext.parasitic-and-friends/
+   */
+  implicit val ec: ExecutionContext = ExecutionContext.parasitic
+
   private def wrapWithETag(resp: Response): ETaggedData[Response] =
     ETaggedData(transformer.rawResponseObjectOf(resp).eTag(), resp)
 
   private def performFetch(
     resourceId: ObjectId,
     reqModifier: Endo[GetObjectRequest.Builder] = identity,
-  )(implicit ec: ExecutionContext): Future[MissingOrETagged[Response]] = {
+  ): Future[MissingOrETagged[Response]] = {
     val requestBuilder = GetObjectRequest.builder().bucket(resourceId.bucket).key(resourceId.key)
     val request = reqModifier(requestBuilder).build()
     s3Client.getObject(request, transformer.asyncResponseTransformer())
@@ -35,9 +44,9 @@ case class S3ObjectFetching[Response](s3Client: S3AsyncClient, transformer: Tran
       }
   }
 
-  def fetch(key: ObjectId)(implicit ec: ExecutionContext): Future[MissingOrETagged[Response]] = performFetch(key)
+  def fetch(key: ObjectId): Future[MissingOrETagged[Response]] = performFetch(key)
 
-  def fetchOnlyIfETagChanged(key: ObjectId, eTag: String)(implicit ec: ExecutionContext): Future[Option[MissingOrETagged[Response]]] =
+  def fetchOnlyIfETagChanged(key: ObjectId, eTag: String): Future[Option[MissingOrETagged[Response]]] =
     performFetch(key, _.ifNoneMatch(eTag)).map(Some(_)).recover {
       case e: S3Exception if e.statusCode == HTTP_NOT_MODIFIED => None // no fresh download because the ETag matched!
     }
@@ -48,5 +57,5 @@ object S3ObjectFetching {
    * Convenience method for creating a fetcher that just returns a simple byte array.
    */
   def byteArraysWith(s3AsyncClient: S3AsyncClient): S3ByteArrayFetching =
-    S3ObjectFetching(s3AsyncClient, Bytes).mapResponse(_.asByteArray())
+    S3ObjectFetching(s3AsyncClient, Bytes).mapResponse(_.asByteArray())(ExecutionContext.parasitic)
 }
