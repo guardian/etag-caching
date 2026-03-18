026868e315bb55d4a0a2adc684043dd924e8a413
Roberto Tyley <roberto.tyley@gmail.com>
2026-03-18 12:27:50 +0000

    Refactor so we can reproduce the flakey test failure
    See https://github.com/guardian/etag-caching/pull/143 - we need to run
    the testcase a large number of times, but not produce excessive logging.

--- core/src/test/scala/com/gu/etagcaching/FreshnessPolicyTest.scala
+++ core/src/test/scala/com/gu/etagcaching/FreshnessPolicyTest.scala
@@ -1,8 +1,9 @@
 package com.gu.etagcaching
 
 import com.github.blemale.scaffeine.{AsyncLoadingCache, Scaffeine}
+import com.gu.etagcaching.DemoCache.withPolicy
 import com.gu.etagcaching.FreshnessPolicy.{AlwaysWaitForRefreshedValue, TolerateOldValueWhileRefreshing}
-import com.gu.etagcaching.fetching.{ETaggedData, Fetching, MissingOrETagged}
+import com.gu.etagcaching.fetching.Fetching
 import com.gu.etagcaching.testkit.TestFetching
 import org.scalatest.OptionValues
 import org.scalatest.concurrent.ScalaFutures
@@ -11,52 +12,73 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.time.SpanSugar._
 
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.ConcurrentLinkedQueue
 import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters._
+import scala.util.control.NonFatal
+
+object DemoCache {
+  def withPolicy(policy: FreshnessPolicy, repeats: Int)(f: DemoCache => Unit): Unit = for (n <- 1 to repeats) {
+    val demoCache = DemoCache(policy)
+    demoCache.log(s"Run $n")
+    try {
+      f(demoCache)
+    } catch {
+      case NonFatal(e) => throw new RuntimeException(demoCache.logs, e)
+    }
+  }
+}
+
+case class DemoCache(policy: FreshnessPolicy) extends ScalaFutures {
+  
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  private val logStore: ConcurrentLinkedQueue[String] = new ConcurrentLinkedQueue[String]()
+
+  def logs: String = logStore.asScala.mkString("\n")
+
+  def log(mess: String): Unit = logStore.add(mess)
+
+  lazy val exampleCache: AsyncLoadingCache[String, Int] = {
+    def simulateWork(task: String, key: String, millis: Long): Unit = {
+      log(s"$task begin: $key")
+      Thread.sleep(millis)
+      log(s"$task end: $key")
+    }
+
+    Scaffeine().buildAsyncFuture[String, Int](
+      loader = key => Future {
+        simulateWork("load", key, 15)
+        0
+      },
+      reloadLoader = Some { case (key, oldValue) => Future {
+        simulateWork("reload", key, 10)
+        oldValue + 1
+      }
+      }
+    )
+  }
+
+  private val reading = policy.on(exampleCache)
+
+  def read(): Int = reading("sample-key").futureValue
+}
 
 class FreshnessPolicyTest extends AnyFlatSpec with Matchers with ScalaFutures with OptionValues {
 
-  case class DemoCache(policy: FreshnessPolicy) {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-    lazy val exampleCache: AsyncLoadingCache[String, Int] = {
-      def simulateWork(task: String, key: String, millis: Long): Unit = {
-        println(s"$task begin: $key")
-        Thread.sleep(millis)
-        println(s"$task end: $key")
-      }
-
-      Scaffeine().buildAsyncFuture[String, Int](
-        loader = key => Future {
-          simulateWork("load", key, 50)
-          0
-        },
-        reloadLoader = Some { case (key, oldValue) => Future {
-          simulateWork("reload", key, 10)
-          oldValue + 1
-        }
-        }
-      )
-    }
-
-    private val reading = policy.on(exampleCache)
-
-    def read() = reading("sample-key").futureValue
-
-  }
-
   "AlwaysWaitForRefreshedValue policy" should "always give us the latest, refreshed value, even tho' it takes some time to make the ETag-checking fetch" in {
-    val demo = DemoCache(AlwaysWaitForRefreshedValue)
-    demo.read() shouldBe 0
-    demo.read() shouldBe 1
+    withPolicy(AlwaysWaitForRefreshedValue, repeats = 1000) { demo =>
+      demo.read() shouldBe 0
+      demo.read() shouldBe 1
+    }
   }
 
   "TolerateOldValueWhileRefreshing policy" should "return instantly if there's an available old value" in {
-    val demo = DemoCache(TolerateOldValueWhileRefreshing)
-
-    demo.read() shouldBe 0
-    failAfter(5.millis) { // should be instant, because we're _not_ waiting for the ETag-checking fetch
+    withPolicy(TolerateOldValueWhileRefreshing, repeats = 1000) { demo =>
       demo.read() shouldBe 0
+      failAfter(4.millis) { // should be instant, because we're _not_ waiting for the ETag-checking fetch
+        demo.read() shouldBe 0
+      }
     }
   }
 
